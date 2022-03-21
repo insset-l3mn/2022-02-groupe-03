ff3003ab2c91b65cca3b16ffa4f37b6032f64d50
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-03-21 11:48:07 +0100

    Update QuestionFacadeREST.java

--- backend/src/main/java/devweb/groupe3/backend/entities/service/QuestionFacadeREST.java
+++ backend/src/main/java/devweb/groupe3/backend/entities/service/QuestionFacadeREST.java
@@ -4,13 +4,16 @@
  */
 package devweb.groupe3.backend.entities.service;
 
+import devweb.groupe3.backend.entities.Proposition;
 import devweb.groupe3.backend.entities.Question;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -18,6 +21,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -35,17 +39,41 @@ public class QuestionFacadeREST extends AbstractFacade<Question> {
     }
 
     @POST
-    @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void create(Question entity) {
-        super.create(entity);
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("/add")
+    public Response create(@FormParam("contenu_question") String contenuQuestion, @FormParam("reponse_question") String reponseQuestion, @FormParam("aideQuestion") String aideQuestion, @FormParam("date_ajouter") Date dateAjouter) {
+        if (contenuQuestion != null && reponseQuestion != null && aideQuestion != null && dateAjouter != null) {
+            Question entityQuestion = new Question();
+            entityQuestion.setContenuQuestion(contenuQuestion);
+            entityQuestion.setReponseQuestion(reponseQuestion);
+            entityQuestion.setAideQuestion(aideQuestion);
+            entityQuestion.setDateAjouter(dateAjouter);
+            super.create(entityQuestion);  
+           return Response.status(200).build();
+        }
+        return Response.status(406).build();
     }
 
     @PUT
-    @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void edit(@PathParam("id") Integer id, Question entity) {
-        super.edit(entity);
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("/update/{id}")
+    public Response edit(@PathParam("id") Integer id, @FormParam("contenu_question") String contenuQuestion, @FormParam("reponse_question") String reponseQuestion, @FormParam("aideQuestion") String aideQuestion, @FormParam("date_ajouter") Date dateAjouter) {
+        Question entity = super.find(id);
+        if (entity != null) {
+            if (contenuQuestion != null) {
+                entity.setContenuQuestion(contenuQuestion);
+            }
+            if (reponseQuestion != null) {
+                entity.setReponseQuestion(reponseQuestion);
+            }
+            if (aideQuestion != null) {
+                entity.setAideQuestion(aideQuestion);
+            }
+            super.edit(entity);
+            return Response.status(200).build();
+        }
+        
+        return Response.status(404).build();
     }
 
     @DELETE
