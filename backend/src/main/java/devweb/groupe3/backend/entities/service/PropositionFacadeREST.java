2f0101d567f6cc8ae20104411db2f247b7846310
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-03-30 10:46:52 +0200

    Update PropositionFacadeREST.java

--- backend/src/main/java/devweb/groupe3/backend/entities/service/PropositionFacadeREST.java
+++ backend/src/main/java/devweb/groupe3/backend/entities/service/PropositionFacadeREST.java
@@ -5,12 +5,15 @@
 package devweb.groupe3.backend.entities.service;
 
 import devweb.groupe3.backend.entities.Proposition;
+import devweb.groupe3.backend.entities.Question;
+import devweb.groupe3.backend.entities.Utilisateur;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -18,6 +21,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -35,23 +39,92 @@ public class PropositionFacadeREST extends AbstractFacade<Proposition> {
     }
 
     @POST
-    @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void create(Proposition entity) {
-        super.create(entity);
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("/add/{id_utilisateur}/{id_competence}")
+    public Response create(@PathParam("id_utilisateur") Integer idUtilisateur, @PathParam("id_question") Integer idQuestion, @FormParam("contenu_proposition") String contenuProposition, @FormParam("validite_proposition") Boolean validiteProposition) {
+        if (idUtilisateur != null) {
+            Utilisateur utilisateur = em.find(Utilisateur.class, idUtilisateur);
+            Question question = em.find(Question.class, idQuestion);
+            if (utilisateur != null && question != null) {
+                if ("admin".equalsIgnoreCase(utilisateur.getTypeUtilisateur())) {
+                    if (contenuProposition != null && validiteProposition != null) {
+                        Proposition proposition = new Proposition();
+                        proposition.setContenuProposition(contenuProposition);
+                        proposition.setValiditeProposition(validiteProposition);
+                        super.create(proposition);
+
+                        return Response.status(200).build();
+                    } else {
+                        return Response.status(404).build();
+                    }
+                } else {
+                    return Response.status(401).build();
+                }
+            } else {
+                return Response.status(404).build();
+            }
+        } else {
+            return Response.status(404).build();
+        }
     }
 
     @PUT
-    @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void edit(@PathParam("id") Integer id, Proposition entity) {
-        super.edit(entity);
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("update/{id_utilisateur}/{id}")
+    public Response edit(@PathParam("id") Integer id, @PathParam("id_utilisateur") Integer idUtilisateur, @FormParam("contenu_proposition") String contenuProposition, @FormParam("validite_proposition") Boolean validiteProposition) {
+        Proposition entity = super.find(id);
+        if (idUtilisateur != null) {
+            Utilisateur utilisateur = em.find(Utilisateur.class, idUtilisateur);
+            if (utilisateur != null) {
+                if ("admin".equalsIgnoreCase(utilisateur.getTypeUtilisateur())) {
+                    if (entity != null) {
+                        if (contenuProposition != null) {
+                            entity.setContenuProposition(contenuProposition);
+                        }
+                        if (validiteProposition != null) {
+                            entity.setValiditeProposition(validiteProposition);
+                        }
+                        super.edit(entity);
+                        return Response.status(200).build();
+                    } else {
+                        return Response.status(404).build();
+
+                    }
+                } else {
+                    return Response.status(401).build();
+                }
+            } else {
+                return Response.status(404).build();
+            }
+        } else {
+            return Response.status(404).build();
+        }
     }
 
     @DELETE
-    @Path("{id}")
-    public void remove(@PathParam("id") Integer id) {
-        super.remove(super.find(id));
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("delete/{id_utilisateur}/{id}")
+    public Response remove(@PathParam("id") Integer id, @PathParam("id_utilisateur") int idUtilisateur) {
+        if (idUtilisateur != 0) {
+            Utilisateur utilisateur = em.find(Utilisateur.class, idUtilisateur);
+            if (utilisateur != null) {
+                if ("admin".equalsIgnoreCase(utilisateur.getTypeUtilisateur())) {
+                    Proposition entity = super.find(id);
+                    if (entity != null) {
+                        super.remove(entity);
+                        return Response.status(200).build();
+                    } else {
+                        return Response.status(404).build();
+                    }
+                } else {
+                    return Response.status(401).build();
+                }
+            } else {
+                return Response.status(404).build();
+            }
+        } else {
+            return Response.status(404).build();
+        }
     }
 
     @GET
@@ -86,5 +159,5 @@ public class PropositionFacadeREST extends AbstractFacade<Proposition> {
     protected EntityManager getEntityManager() {
         return em;
     }
-    
+
 }
